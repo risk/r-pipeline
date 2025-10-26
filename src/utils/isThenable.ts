e339932801f10527568ba487902afe8c7e6f20b6
Noboru Takahashi <risk.masu@gmail.com>
2025-10-26 09:15:53 +0900

    isThenable tweaks

--- src/utils/isThenable.ts
+++ src/utils/isThenable.ts
@@ -3,6 +3,11 @@
  * https://github.com/risk/r-pipeline
  */
 
-export function isThenable<T>(x: unknown): x is Promise<T> | { then: Function } {
-  return !!x && (typeof x === 'object' || typeof x === 'function') && typeof (x as any).then === 'function'
+export function isThenable<T = unknown>(x: unknown): x is Promise<T> | { then: (...args: unknown[]) => unknown } {
+  return (
+    !!x &&
+    (typeof x === 'object' || typeof x === 'function') &&
+    'then' in x &&
+    typeof (x as { then: unknown }).then === 'function'
+  )
 }
